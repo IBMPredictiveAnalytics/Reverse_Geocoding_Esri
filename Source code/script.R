902fea7d8d046f7b341dda2ee6172ac0666c3560
gfilla <gfilla23@gmail.com>
2015-10-13 10:19:15 -0500

    fixed extension - adding materials
    Fixed and updated extension to accept username/password and radius
    argument for distance.  Adding screenshots and example files.  Updating
    readme file as follow up.

--- Source code/script.R
+++ Source code/script.R
@@ -1,43 +1,155 @@
+# Reverse Geocoding: Get the address that corresponds to a given latitude and a given longitude 
+# Node Developed by IBM
 
-# Install function for packages (automatic)
-packages <- function(x){
-  x <- as.character(match.call()[[2]])
-  if (!require(x,character.only=TRUE)){
-    install.packages(pkgs=x,repos="http://cran.r-project.org")
-    require(x,character.only=TRUE)
+# Packages used : 
+# 'plyr' author : Hadley Wickham
+#	'RCurl' author : Duncan Temple Lang 
+# 'RJSONIO' author : Duncan Temple Lang         
+
+# Install packages if necessary
+
+if (require("plyr")) {
+  print("plyr is loaded correctly")
+} else {
+  print("trying to install plyr")
+  install.packages("plyr")
+  if (require(plyr)) {
+    print("plyr installed and loaded")
+  } else {
+    stop("Could not install required packages (plyr)")
   }
 }
+library(plyr)
 
-packages(RCurl)
-packages(RJSONIO)
-packages(plyr)
+if (require("RCurl")) {
+  print("RCurl is loaded correctly")
+} else {
+  print("trying to install RCurl")
+  install.packages("RCurl")
+  if (require(RCurl)) {
+    print("RCurl installed and loaded")
+  } else {
+    stop("Could not install required packages (RCurl)")
+  }
+}
+library(RCurl)  
 
-latitude <- modelerData$%%lat%%
-  longitude<-modelerData$%%lon%%
-  token<-'%%token%%'
-print(location)
-root <- "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode"
-u <- paste(root,  "?location=",lat,'%2C+',lon,'&distance=200&outSR=', "&token=", token, "&f=json", sep = "")
-u <- gsub(' ','%20',u) #Encode URL Parameters
-print(u)
+if (require("RJSONIO")) {
+  print("RJSONIO is loaded correctly")
+} else {
+  print("trying to install RJSONIO")
+  install.packages("RJSONIO")
+  if (require(RJSONIO)) {
+    print("RJSONIO installed and loaded")
+  } else {
+    stop("Could not install required packages (RJSONIO)")
+  }
+}
+library(RJSONIO) 
+library(httr)
+#Generate Token
+pwd <- '%%pwd%%'
+url = "https://arcgis.com/sharing/rest/generateToken"
+data = list('username'= "%%username%%",
+            'password'= pwd,
+            'referer' = 'http://arcgis.com',
+            'expiration' = 1209600,
+            'f'= 'json')
+r<-POST(url,body = data)
+content(r)
+x <- fromJSON(content(r))
+token<-x$token
 
-doc <- aaply(u,1,getURL)
-json <- alply(doc,1,fromJSON,simplify = FALSE)
-coord = laply(json,function(x) {
+# forming URL address
+lat <- modelerData$%%identif_lat%%
+  long<-modelerData$%%identif_lng%%
   
-  lat <- x$locations[[1]]$feature$geometry$x
-  lng <- x$locations[[1]]$feature$geometry$y
-  return(c(lat,lng))
-  
-})
-lng<-c(coord[,1])
-lat<-c(coord[,2])
-modelerData<-cbind(modelerData,lat)
-print(modelerData)
-var1<-c(fieldName="Latitude",fieldLabel="",fieldStorage="real",fieldFormat="",fieldMeasure="",  fieldRole="")
+  root <- "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode"
+url <- paste(root,  "?location=",long,'%2C+',lat,'&distance=', %%dist%%,'&outSR=', "&token=", token, "&f=json", sep = "")
+
+print(url)
+
+# This function takes a URL list and returns a dataframe with lat long and status
+getAddress <- function(url) {
+  # performing GET on url
+  doc <- llply(url, getURL)
+  # converting jso into list
+  json <- llply(doc, fromJSON, simplify = FALSE)
+  # Getting lat and long fields from the json object
+  coord = ldply(json, function(x) {
+    
+    Address<-x$address$Address
+    Neighborhood<-x$address$Neighborhood
+    City<-x$address$City
+    Subregion<-x$address$Subregion
+    Region<-x$address$Region
+    Postal<-x$address$Postal
+    PostalExt<-x$address$PostalExt
+    CountryCode<-x$address$CountryCode
+    Match_addr<-x$address$Match_addr
+    
+    Address[is.null(Address)] <- "empty"
+    Neighborhood[is.null(Neighborhood)] <- "empty"
+    City[is.null(City)] <- "empty"
+    Subregion[is.null(Subregion)] <- "empty"
+    Region[is.null(Region)] <- "empty"
+    Postal[is.null(Postal)] <- "empty"
+    PostalExt[is.null(PostalExt)] <- "empty"
+    CountryCode[is.null(CountryCode)] <- "empty"
+    Match_addr[is.null(Match_addr)] <- "empty"
+    
+    return(c(Address, Neighborhood, City, Subregion, Region, Postal, PostalExt, CountryCode, Match_addr))
+    
+  }
+  )
+  return(coord)
+}
+
+coord <- getAddress(url)
+
+Address<-c(coord[,1])
+Neighborhood<-c(coord[,2])
+City<-c(coord[,3])
+Subregion<-c(coord[,4])
+Region<-c(coord[,5])
+Postal<-c(coord[,6])
+PostalExt<-c(coord[,7])
+CountryCode<-c(coord[,8])
+Match_addr<-c(coord[,9])
+
+
+modelerData<-cbind(modelerData,Address)
+var1<-c(fieldName="Address",fieldLabel="",fieldStorage="string",fieldFormat="",fieldMeasure="",  fieldRole="")
 modelerDataModel<-data.frame(modelerDataModel,var1)
-modelerData<-cbind(modelerData,lng)
-print(modelerData)
-var2<-c(fieldName="Longitude",fieldLabel="",fieldStorage="real",fieldFormat="",fieldMeasure="",  fieldRole="")
+
+modelerData<-cbind(modelerData,Neighborhood)
+var2<-c(fieldName="Neighborhood",fieldLabel="",fieldStorage="string",fieldFormat="",fieldMeasure="",  fieldRole="")
 modelerDataModel<-data.frame(modelerDataModel,var2)
 
+modelerData<-cbind(modelerData,City)
+var3<-c(fieldName="City",fieldLabel="",fieldStorage="string",fieldFormat="",fieldMeasure="",  fieldRole="")
+modelerDataModel<-data.frame(modelerDataModel,var3)
+
+modelerData<-cbind(modelerData,Subregion)
+var4<-c(fieldName="Subregion",fieldLabel="",fieldStorage="string",fieldFormat="",fieldMeasure="",  fieldRole="")
+modelerDataModel<-data.frame(modelerDataModel,var4)
+
+modelerData<-cbind(modelerData,Region)
+var5<-c(fieldName="Region",fieldLabel="",fieldStorage="string",fieldFormat="",fieldMeasure="",  fieldRole="")
+modelerDataModel<-data.frame(modelerDataModel,var5)
+
+modelerData<-cbind(modelerData,Postal)
+var6<-c(fieldName="Postal",fieldLabel="",fieldStorage="string",fieldFormat="",fieldMeasure="",  fieldRole="")
+modelerDataModel<-data.frame(modelerDataModel,var6)
+
+modelerData<-cbind(modelerData,PostalExt)
+var7<-c(fieldName="PostalExt",fieldLabel="",fieldStorage="string",fieldFormat="",fieldMeasure="",  fieldRole="")
+modelerDataModel<-data.frame(modelerDataModel,var7)
+
+modelerData<-cbind(modelerData,CountryCode)
+var8<-c(fieldName="CountryCode",fieldLabel="",fieldStorage="string",fieldFormat="",fieldMeasure="",  fieldRole="")
+modelerDataModel<-data.frame(modelerDataModel,var8)
+
+modelerData<-cbind(modelerData,Match_addr)
+var9<-c(fieldName="Match_addr",fieldLabel="",fieldStorage="string",fieldFormat="",fieldMeasure="",  fieldRole="")
+modelerDataModel<-data.frame(modelerDataModel,var9)
